23fa333fbb752ca84ddde6c36a5008e9d6028a2c
coalizao016-creator <coalizao016@gmail.com>
2025-08-25 11:33:38 -0300

    att

---
+++ mcs.js
@@ -0,0 +1,174 @@
+// mcs.js
+const mcs = [
+  {
+    id: "MC001",
+    nome: "PUMA",
+    imagem: "images/MC001.png",
+    cidade: "São Carlos - SP",
+    titulo: "🏆 Liga Caipira 2025",
+    links: [
+      { nome: "Instagram", url: "https://www.instagram.com/pumaemici/" }
+    ]
+  },
+  {
+    id: "MC002",
+    nome: "Guri",
+    imagem: "images/MC002.png",
+    cidade: "Ribeirão Preto - SP",
+    titulo: "🔥 Destaque",
+    links: [
+      { nome: "Instagram", url: "https://www.instagram.com/oi_guri/" },
+      { nome: "YouTube", url: "https://www.youtube.com/@Gurimc" },
+      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/5I3oLG0SOgiA12K1W0gE0s" }
+    ]
+  },
+  {
+    id: "MC003",
+    nome: "Bubba",
+    imagem: "images/MC003.png",
+    cidade: "Ribeirão Preto - SP",
+    titulo: "🏆 Ascensão dos Monstros",
+    links: [
+      { nome: "Instagram", url: "https://www.instagram.com/bubbassalto/" },
+      { nome: "YouTube", url: "https://www.youtube.com/@bubbassalto" },
+      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/5FI9nwGy8otx6wZ8drZ1l8?si=YtjJJQYLRpK75Hga0bBa2w" }
+    ]
+  },
+  {
+    id: "MC004",
+    nome: "Dieguim",
+    imagem: "images/MC004.png",
+    cidade: "Batatais - SP",
+    titulo: "🎤 ",
+    links: [
+      { nome: "Instagram", url: "https://www.instagram.com/diego_rrxdrigues/" }
+    ]
+  },
+  {
+    id: "MC005",
+    nome: "Last",
+    imagem: "images/MC005.png",
+    cidade: "Franca - SP",
+    titulo: "🔥",
+    links: [
+      { nome: "Instagram", url: "https://www.instagram.com/_last_mc/" },
+      { nome: "Youtube", url: "https://www.youtube.com/@LastMc" }
+    ]
+  },
+  {
+    id: "MC006",
+    nome: "Haiki",
+    imagem: "images/MC006.png",
+    cidade: "Batatais - SP",
+    titulo: "🏆 Campeão Batalha da Vinte",
+    links: [
+      { nome: "Instagram", url: "#" },
+      { nome: "YouTube", url: "#" }
+    ]
+  },
+  {
+    id: "MC007",
+    nome: "Luh do click",
+    imagem: "images/MC007.png",
+    cidade: "Araraquara - SP",
+    titulo: "🎶 Voz da Resistência",
+    links: [
+      { nome: "Instagram", url: "#" },
+      { nome: "Spotify", url: "#" }
+    ]
+  },
+  {
+    id: "MC008",
+    nome: "Jovem Kata",
+    imagem: "images/MC008.png",
+    cidade: "Jaboticabal - SP",
+    titulo: "✒️ Poesia de Rua",
+    links: [
+      { nome: "Instagram", url: "#" }
+    ]
+  },
+  {
+    id: "MC009",
+    nome: "Jimmy",
+    imagem: "images/MC009.png",
+    cidade: "Barretos - SP",
+    titulo: "🔥 Destaque Regional",
+    links: [
+      { nome: "Instagram", url: "#" },
+      { nome: "YouTube", url: "#" }
+    ]
+  },
+  {
+    id: "MC010",
+    nome: "Tyrano",
+    imagem: "images/MC010.png",
+    cidade: "Matão - SP",
+    titulo: "🤫 Punchline Mortal",
+    links: [
+      { nome: "Instagram", url: "#" }
+    ]
+  },
+  {
+    id: "MC011",
+    nome: "Shazzam",
+    imagem: "images/MC011.png",
+    cidade: "Cravinhos - SP",
+    titulo: "🏆 Melodia e Protesto",
+    links: [
+      { nome: "Instagram", url: "#" },
+      { nome: "Spotify", url: "#" }
+    ]
+  },
+  {
+    id: "MC012",
+    nome: "Jhow",
+    imagem: "images/MC012.png",
+    cidade: "Brodowski - SP",
+    titulo: "⏳ Mestre do Tempo",
+    links: [
+      { nome: "Instagram", url: "#" }
+    ]
+  },
+  {
+    id: "MC013",
+    nome: "MS",
+    imagem: "images/MC013.png",
+    cidade: "Orlândia - SP",
+    titulo: "💡 Flow Consciente",
+    links: [
+      { nome: "Instagram", url: "#" },
+      { nome: "YouTube", url: "#" }
+    ]
+  },
+  {
+    id: "MC014",
+    nome: "Giroto",
+    imagem: "images/MC014.png",
+    cidade: "Monte Alto - SP",
+    titulo: "🤠 O Caipira do Verso",
+    links: [
+      { nome: "Instagram", url: "#" }
+    ]
+  },
+  {
+    id: "MC015",
+    nome: "Zemaki",
+    imagem: "images/MC015.png",
+    cidade: "Pontal - SP",
+    titulo: "🌱 Raiz do Interior",
+    links: [
+      { nome: "Instagram", url: "#" },
+      { nome: "TikTok", url: "#" }
+    ]
+  },
+  {
+    id: "MC016",
+    nome: "Satsu",
+    imagem: "images/MC016.png",
+    cidade: "Serrana - SP",
+    titulo: "👻 Rima Fantasma",
+    links: [
+      { nome: "Instagram", url: "#" }
+    ]
+  }
+];
