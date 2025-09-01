8b4dc181f53e544388a8552416499055417a6def
coalizao016-creator <coalizao016@gmail.com>
2025-09-01 10:01:25 -0300

    EDIT MCS

--- mcs.js
+++ mcs.js
@@ -11,266 +11,7 @@ const mcs = [
       { nome: "Instagram", url: "https://www.instagram.com/pumaemici/" }
     ]
   },
-  {
-    id: "MC002",
-    nome: "Guri",
-    imagem: "images/MC002.png",
-    cidade: "Ribeirão Preto - SP",
-    titulo: "🔥 Destaque",
-    ranking: 2,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/oi_guri/" },
-      { nome: "YouTube", url: "https://www.youtube.com/@Gurimc" },
-      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/5I3oLG0SOgiA12K1W0gE0s" }
-    ]
-  },
-  {
-    id: "MC003",
-    nome: "Bubba",
-    imagem: "images/MC003.png",
-    cidade: "Ribeirão Preto - SP",
-    titulo: "🏆 Ascensão dos Monstros",
-    ranking: 3,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/bubbassalto/" },
-      { nome: "YouTube", url: "https://www.youtube.com/@bubbassalto" },
-      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/5FI9nwGy8otx6wZ8drZ1l8?si=YtjJJQYLRpK75Hga0bBa2w" }
-    ]
-  },
-  {
-    id: "MC004",
-    nome: "Dieguim",
-    imagem: "images/MC004.png",
-    cidade: "Batatais - SP",
-    titulo: "",
-    ranking: 16,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/diego_rrxdrigues/" }
-    ]
-  },
-  {
-    id: "MC005",
-    nome: "Last",
-    imagem: "images/MC005.png",
-    cidade: "Franca - SP",
-    titulo: "",
-    ranking: 11,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/_last_mc/" },
-      { nome: "Youtube", url: "https://www.youtube.com/@LastMc" }
-    ]
-  },
-  {
-    id: "MC006",
-    nome: "Haiki",
-    imagem: "images/MC006.png",
-    cidade: "Ribeirão Preto - SP",
-    titulo: "",
-    ranking: 12,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/mc_haiki/" },
-      { nome: "YouTube", url: "#" }
-    ]
-  },
-  {
-    id: "MC007",
-    nome: "Luh do click",
-    imagem: "images/MC007.png",
-    cidade: "Pradópolis - SP",
-    titulo: "campeã regional diver 016 ",
-    ranking: 6,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/luhdoclick/" },
-      { nome: "Spotify", url: "#" }
-    ]
-  },
-  {
-    id: "MC008",
-    nome: "Jovem Kata",
-    imagem: "images/MC008.png",
-    cidade: "Ribeirão Preto - SP",
-    titulo: "",
-    ranking: 8,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/jovemkata_/" },
-      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/0fSHyBl7abxUvS28TfaikC" },
-      { nome: "Youtube", url: "https://www.youtube.com/@jovemkata" }
-    ]
-  },
-  {
-    id: "MC009",
-    nome: "Jimmy",
-    imagem: "images/MC009.png",
-    cidade: "Ribeirão Preto - SP",
-    titulo: "",
-    ranking: 7,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/eojimmy_/" },
-      { nome: "YouTube", url: "#" }
-    ]
-  },
-  {
-    id: "MC010",
-    nome: "Tyrano",
-    imagem: "images/MC010.png",
-    cidade: "Franca - SP",
-    titulo: "",
-    ranking: 5,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/tyranoofc/" },
-      { nome: "Youtube", url: "https://www.youtube.com/@tyranoofc" },
-      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/6Bl1DUWF01Run0eT4mG5Iw" }
-    ]
-  },
-  {
-    id: "MC011",
-    nome: "Shazzam",
-    imagem: "images/MC011.png",
-    cidade: "Araraquara - SP",
-    titulo: "",
-    ranking: 14,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/oshazzam_/" },
-      { nome: "Spotify", url: "#" }
-    ]
-  },
-  {
-    id: "MC012",
-    nome: "Jhow",
-    imagem: "images/MC012.png",
-    cidade: "Araraquara - SP",
-    titulo: " ",
-    ranking: 9,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/jhowfidelis777/" },
-      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/5DF3n5UZ4CaQ38s4E0YQZV" }
-    ]
-  },
-  {
-    id: "MC013",
-    nome: "MS",
-    imagem: "images/MC013.png",
-    cidade: "Bauru - SP",
-    titulo: "  ",
-    ranking: 15,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/ms.ofc/" },
-      { nome: "YouTube", url: "#" }
-    ]
-  },
-  {
-    id: "MC014",
-    nome: "Giroto",
-    imagem: "images/MC014.png",
-    cidade: "Ribeirão Preto - SP",
-    titulo: "",
-    ranking: 13,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/girotomc/" }
-    ]
-  },
-  {
-    id: "MC015",
-    nome: "Zemaki",
-    imagem: "images/MC015.png",
-    cidade: "São Carlos - SP",
-    titulo: " ",
-    ranking: 10,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/_zemaki/" },
-      { nome: "TikTok", url: "#" }
-    ]
-  },
-  {
-    id: "MC016",
-    nome: "Satsu",
-    imagem: "images/MC016.png",
-    cidade: "São Carlos - SP",
-    titulo: "",
-    ranking: 4,
-    links: [
-      { nome: "Instagram", url: "https://www.instagram.com/satsumc/" },
-      { nome: "Spotify", url: "https://open.spotify.com/intl-pt/artist/3Av64XJ3EDhahcI36vBZ2A" }
-    ]
-  },
-  {
-    id: "MC017",
-    nome: "Marcola mc",
-    imagem: "images/MC017.png",
-    cidade: "Morro agudo",
-    titulo: "🔥 Destaque",
-    ranking: null,
-    links: [
-      {
-        nome: "Instagram",
-        url: "https://www.instagram.com/marcolaa.st?igsh=MTcybHdjN3p5a3Nyag=="
-      }
-    ]
-  },
-  {
-    id: "MC018",
-    nome: "Mancha",
-    imagem: "images/MC018.png",
-    cidade: "Taquaritinga-SP",
-    titulo: "🏆 da Batalha dos Brodi",
-    ranking: null,
-    links: [
-      {
-        nome: "Instagram",
-        url: "https://www.instagram.com/esqacmvms?igsh=MTJxYzkxZjBpZnJ5MQ=="
-      },
-      {
-        nome: "YouTube",
-        url: "https://youtube.com/@mancha016?si=y7QF5UzGI8mxqgfB"
-      }
-    ]
-  },
-  {
-    id: "MC019",
-    nome: "Marin",
-    imagem: "images/MC019.png",
-    cidade: "Sales Oliveira",
-    titulo: "🏆 Campeão da temporada da bdvinte",
-    ranking: null,
-    links: [
-      {
-        nome: "Instagram",
-        url: "https://www.instagram.com/kaikymarin?igsh=cWplcmtqY2Y5NmVt&utm_source=qr"
-      }
-    ]
-  },
-  {
-    id: "MC020",
-    nome: "Ronaldy",
-    imagem: "images/MC020.png",
-    cidade: "São Joaquim da Barra",
-    titulo: "🏆 da Conexão de Rimas, fumo muito e rimo mais ainda",
-    ranking: null,
-    links: [
-      {
-        nome: "Instagram",
-        url: "https://www.instagram.com/ronaldy.wav?igsh=NXIxaTZmMGV0eXpp&utm_source=qr"
-      },
-      {
-        nome: "YouTube",
-        url: "https://youtube.com/@kbz.ronaldy?si=o38It-ijAhCK7cjY"
-      }
-    ]
-  },
-  {
-    id: "MC021",
-    nome: "Nikito MC",
-    imagem: "images/MC021.png",
-    cidade: "Ribeirão Preto - SP",
-    titulo: "🏆 Campeão da batalha sangue na 7",
-    ranking: null,
-    links: [
-      {
-        nome: "Instagram",
-        url: "https://www.instagram.com/nikito_mc00"
-      }
-    ]
-  },
+  // ... (MCs 002 a 022, sem alteração)
   {
     id: "MC022",
     nome: "NGK",
@@ -297,13 +38,50 @@ const mcs = [
     id: "MC023",
     nome: "F.D",
     imagem: "images/MC023.png",
-    cidade: "Ribeirão Preto",
+    cidade: "Ribeirão Preto - SP",
     titulo: "🏆 Campeão em 3 cidades",
     ranking: null,
     links: [
       {
         nome: "Instagram",
         url: "https://www.instagram.com/felipebrandaoporto/"
+      }
+    ]
+  },
+  // ===== NOVOS MCS ADICIONADOS AQUI =====
+  {
+    id: "MC024",
+    nome: "Gomez",
+    imagem: "images/MC024.png",
+    cidade: "Araraquara - SP",
+    titulo: "Aquele lá do \"eu começo\" 🦅",
+    ranking: null,
+    links: [
+      {
+        nome: "Instagram",
+        url: "https://www.instagram.com/itsmegomezs?igsh=c3VoN2RoeDE0aWE0"
+      }
+    ]
+  },
+  {
+    id: "MC025",
+    nome: "Arteq",
+    imagem: "images/MC025.png",
+    cidade: "Ribeirão Preto - SP",
+    titulo: "🏆 Múltiplo campeão em Ribeirão Preto",
+    ranking: null,
+    links: [
+      {
+        nome: "Instagram",
+        url: "https://www.instagram.com/arteqmarco?igsh=MWs3eXFvOTQ0dGx1NA=="
+      },
+      {
+        nome: "YouTube",
+        url: "https://youtube.com/@arteqoficial8551?si=U_FJzwCXgXn1OSz2"
+      },
+      {
+        nome: "Spotify",
+        url: "https://open.spotify.com/artist/7bMwJ2OmaTmIs9rFFZQlq2?si=Kr2ogEk3TF6Ad6ZStveDqw"
       }
     ]
   }
